fe63993bf350c4c079d33b38f5d4a5c8a61a3370
whesllysowza-lgtm <whesllysowza@gmail.com>
2026-09-24 23:00:28 +0000

    Ativar contas individuais e paleta completa

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
+import AuthGate from "./components/AuthGate";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -32,7 +33,7 @@ function App() {
       >
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <AuthGate><Router /></AuthGate>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
